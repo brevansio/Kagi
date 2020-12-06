65e236f6624f92a945322761df9e5ccbc695a3e2
Bruce Evans <bruce.evans.dev@gmail.com>
2020-12-06 15:51:22 +0900

    [Autofill] Clean up comments

--- KagiAutoFill/CredentialProviderViewController.swift
+++ KagiAutoFill/CredentialProviderViewController.swift
@@ -86,35 +86,15 @@ class CredentialProviderViewController: ASCredentialProviderViewController {
         self.extensionContext.completeRequest(withSelectedCredential: credential, completionHandler: nil)
     }
 
-    /*
-     Prepare your UI to list available credentials for the user to choose from. The items in
-     'serviceIdentifiers' describe the service the user is logging in to, so your extension can
-     prioritize the most relevant credentials in the list.
-    */
     override func prepareCredentialList(for serviceIdentifiers: [ASCredentialServiceIdentifier]) {
     }
 
-    /*
-     Implement this method if your extension supports showing credentials in the QuickType bar.
-     When the user selects a credential from your app, this method will be called with the
-     ASPasswordCredentialIdentity your app has previously saved to the ASCredentialIdentityStore.
-     Provide the password by completing the extension request with the associated ASPasswordCredential.
-     If using the credential would require showing custom UI for authenticating the user, cancel
-     the request with error code ASExtensionError.userInteractionRequired.
-    */
     override func provideCredentialWithoutUserInteraction(for credentialIdentity: ASPasswordCredentialIdentity) {
         self.extensionContext.cancelRequest(withError: NSError(domain: ASExtensionErrorDomain,
                                                                code: ASExtensionError.userInteractionRequired.rawValue))
     }
 
-    /*
-     Implement this method if provideCredentialWithoutUserInteraction(for:) can fail with
-     ASExtensionError.userInteractionRequired. In this case, the system may present your extension's
-     UI and call this method. Show appropriate UI for authenticating the user then provide the password
-     by completing the extension request with the associated ASPasswordCredential.
-    */
     override func prepareInterfaceToProvideCredential(for credentialIdentity: ASPasswordCredentialIdentity) {
-        // TODO: Show PIN if needed
         // Show File Picker
         let documentPicker = UIDocumentPickerViewController(documentTypes: ["public.item", "public.content"], in: .open)
         documentPicker.delegate = self
